b5d7dd046eacc4721af374952ea6f680ceb4314b
Kai :) <2200182@schoolsnet.act.edu.au>
2026-09-05 08:17:22 +1000

    Enhance turtle graphics with cloud and moon functions
    Added functions to draw clouds and a moon with random colors and sizes.

--- starting_code.py
+++ starting_code.py
@@ -1,7 +1,6 @@
 import turtle
 import random
 
-# ---------- Setup ----------
 t = turtle.Turtle()
 screen = turtle.Screen()
 t.shape("turtle")
@@ -9,7 +8,7 @@ t.speed(0)
 t.pensize(1)
 t.pencolor("black")
 t.fillcolor("orange")
-screen.bgcolor("skyblue")
+screen.bgcolor("darkblue")
 screen.setup(1000, 500)
 
 def go(x, y):
@@ -19,14 +18,52 @@ def go(x, y):
 
 def five_pointed_star():
     t.begin_fill()
+
+    colour = random.choice(["orange","yellow","gold"])
+    t.fillcolor(colour)
+
+    side_length = random.randint(50,100)
     for i in range(5):
-        t.forward(100)
+        t.forward(side_length)
         t.left(215)
+    
     t.end_fill()
 
-for i in range(30):
-    go(random.randint(-500,500),random.randint(-250,250))
-    five_pointed_star()
+def cloud():
+    t.fillcolor("lightsteelblue")
+    t.setheading(0)
+    t.begin_fill()
+    for i in range(18):
+        t.forward(8)
+        t.left(20)
+    t.forward(50)
+    for i in range(18):
+        t.forward(13)
+        t.left(20)
+    t.forward(50)
+    for i in range(18):
+        t.forward(8)
+        t.left(20)
+    t.left(90)
+    t.forward(20)
+    for i in range(18):
+        t.forward(4)
+        t.left(20)
+    t.end_fill()
+
+def draw_moon():
+    t.setheading(0)
+    t.fillcolor("azure")
+    t.begin_fill()
+    for i in range(18):
+        t.forward(20)
+        t.left(10)
+    t.forward(85)
+    t.right(180)
+    for i in range(18):
+        t.forward(20)
+        t.right(10)
+    t.end_fill()
 
 
 
